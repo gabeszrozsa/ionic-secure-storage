6f2edc92ae1b6928d9f125fd3702e369e7fce157
Gabesz Rozsa <gabesz.rozsa@gmail.com>
2022-07-13 14:26:44 +0200

    MVP

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SecureStorage } from '@ionic-native/secure-storage/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  constructor(
+    private secureStorage: SecureStorage,
+    private platform: Platform
+  ) {}
 
-  constructor() {}
+  async read() {
+    await this.platform.ready();
 
+    const secureStorage = await this.secureStorage.create('my_store_name');
+
+    // try {
+    //   const result = await secureStorage.set('key', 'value!!!');
+    //   alert('Key set' + JSON.stringify(result));
+    // } catch (error) {
+    //   alert('set error: ' + JSON.stringify(error));
+    // }
+
+    try {
+      const result = await secureStorage.get('key');
+      alert('Key get' + JSON.stringify(result));
+    } catch (error) {
+      alert('get error: ' + JSON.stringify(error));
+    }
+  }
+
+  async write(value: string) {
+    await this.platform.ready();
+
+    const secureStorage = await this.secureStorage.create('my_store_name');
+
+    try {
+      const result = await secureStorage.set('key', value);
+      alert('Key set' + JSON.stringify(result));
+    } catch (error) {
+      alert('set error: ' + JSON.stringify(error));
+    }
+  }
 }
